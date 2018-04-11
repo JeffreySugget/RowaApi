ca96a9fe8612362e131be44071276a7f0bed08db
jeffrey.sugget <jeffrey.sugget@gmail.com>
2018-04-11 09:18:51 +0800

    Added User Controller

---
+++ Rowa.Api/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Rowa.Api.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult GetUser()
+        {
+            return Ok("This will get a user");
+        }
+    }
+}
