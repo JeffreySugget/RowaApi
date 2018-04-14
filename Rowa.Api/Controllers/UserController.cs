18af6035f7a5d9c69e49c0227be5842a2edf7c74
jeffrey.sugget <jeffrey.sugget@gmail.com>
2018-04-14 09:05:07 +0800

    Added create user method. Setup DI

--- Rowa.Api/Controllers/UserController.cs
+++ Rowa.Api/Controllers/UserController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using Rowa.Repository.Interfaces;
+using Rowa.Api.Models;
 
 namespace Rowa.Api.Controllers
 {
     public class UserController : ApiController
     {
+        private IUserRepository _userRepository;
+        private ICommonMethods _commonMethods;
+
+        public UserController(IUserRepository userRepository, ICommonMethods commonMethods)
+        {
+            _userRepository = userRepository;
+            _commonMethods = commonMethods;
+        }
+
+        [HttpGet]
         public IHttpActionResult GetUser()
         {
             return Ok("This will get a user");
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateUser([FromBody] UserModel userModel)
+        {
+            var newUser = new Repository.Models.User
+            {
+                Username = userModel.Username,
+                Password = userModel.Password
+            };
+
+            return Ok(_userRepository.Add(newUser));
         }
     }
 }
