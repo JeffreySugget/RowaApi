663b2bc8f22209a9c3f4f46f0cb67a63150ad446
Jeff Sugget <jeffrey.sugget@gmail.com>
2018-04-26 10:42:52 +0800

    Created profile controller

---
+++ Rowa.Api/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Rowa.Api.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    [RoutePrefix("api/user")]
+    public class ProfileController : ApiController
+    {
+
+    }
+}
