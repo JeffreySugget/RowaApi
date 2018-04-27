4f507ef0812f5061a32547868feed198851e60a6
Jeff Sugget <jeffrey.sugget@gmail.com>
2018-04-27 10:35:45 +0800

    Created members controller

---
+++ Rowa.Api/Controllers/MembersController.cs
@@ -0,0 +1,24 @@
+﻿using Rowa.Api.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Rowa.Api.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    [RoutePrefix("api/profile")]
+    public class MembersController : ApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUserInformationRepository _userInformationRepository;
+
+        public MembersController(IUserRepository userRepository, IUserInformationRepository userInformationRepository)
+        {
+            _userRepository = userRepository;
+            _userInformationRepository = userInformationRepository;
+        }
+    }
+}
